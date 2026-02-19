32f2f37826cf934f97cb6e4848746e04e0a12f43
H6LS1S <seagull.bogdan@gmail.com>
2026-02-19 15:44:32 +0200

    feat: added gzip-ed html build

--- main/server.c
+++ main/server.c
@@ -13,8 +13,8 @@ static volatile int sse_fd = -1;
 
 httpd_handle_t http_server = NULL;
 
-extern const uint8_t index_html_start[] asm("_binary_index_html_start");
-extern const uint8_t index_html_end[] asm("_binary_index_html_end");
+extern const uint8_t index_html_start[] asm("_binary_index_html_gz_start");
+extern const uint8_t index_html_end[] asm("_binary_index_html_gz_end");
 static const char sse_resp[] = "HTTP/1.1 200 OK\r\n"
 		"Cache-Control: no-store\r\n"
 		"Content-Type: text/event-stream\r\n"
@@ -22,16 +22,10 @@ static const char sse_resp[] = "HTTP/1.1 200 OK\r\n"
 		"retry: 20000\r\n"
 		"\r\n";
 
-static esp_err_t not_found_handler(httpd_req_t *req, httpd_err_code_t err) {
-	httpd_resp_set_status(req, "303 See Other");
-	httpd_resp_set_hdr(req, "Location", "/");
-	httpd_resp_send(req, "Redirect to the captive portal", HTTPD_RESP_USE_STRLEN);
-	return ESP_OK;
-}
-
 esp_err_t index_handler(httpd_req_t *req) {
 	httpd_resp_set_type(req, "text/html");
-	httpd_resp_send(req, (const char *) index_html_start,  index_html_end - index_html_start);
+	httpd_resp_set_hdr(req, "Content-Encoding", "gzip");
+	httpd_resp_send(req, (const char *) index_html_start, index_html_end - index_html_start);
 	return ESP_OK;
 }
 
@@ -124,8 +118,6 @@ void server_start(void) {
 	if (httpd_start(&http_server, &config) != ESP_OK) return;
 
 	ESP_LOGI(TAG, "Registering URI handlers");
-
-	httpd_register_err_handler(http_server, HTTPD_404_NOT_FOUND, not_found_handler);
 
 	httpd_uri_t index_uri = {.uri = "/", .method = HTTP_GET, .handler = index_handler};
 	httpd_register_uri_handler(http_server, &index_uri);
